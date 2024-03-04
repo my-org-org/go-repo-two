aa3c738f8f9958a8a712eab135dc2dca965921f8
TarasHrynchuk <125279218+TarasHrynchuk@users.noreply.github.com>
2024-03-04 13:24:56 +0100

    v1.0.3

--- dependency_consumer/dependency_consumer.go
+++ dependency_consumer/dependency_consumer.go
@@ -3,9 +3,12 @@ package dependency_consumer
 import (
 	"fmt"
 	"github.com/my-org-for-test/go-repo-two/internal"
+	"github.com/my-org-for-test/go-repo-one/dependency"
 )
 
 func PrintDependencyMessage() {
-	fmt.Println("go-repo-two. Release 2 !!!!!!!!!!!! : Dependency Consumer")
+	fmt.Println("go-repo-two. Release 3 $$$$$ : Use 1st repo")
+	dependency.PrintDependencyMessage()
+	fmt.Println("go-repo-two. Release 3 !!!!!!!!!!!! : Dependency Consumer")
     internal.InternalFunction()
 }
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    fmt.Println("========> go-repo-tro: Release 2")
+    fmt.Println("========> go-repo-tro: Release 3")
 
 	fmt.Println("go-repo-two: Dependency on go-repo-one:")
 	dependency.PrintDependencyMessage()
